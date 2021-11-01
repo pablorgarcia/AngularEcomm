1c22d3a4659ae119df12d68303f660498684c811
pablorgarcia <pablo.ramon.garcia.fernandez@gmail.com>
2021-11-01 02:32:14 +0100

    product category service and funct created

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductCategory } from 'src/app/models/product-category';
 import { ProductService } from 'src/app/services/product.service';
+import { ProductCategoryService } from 'src/app/services/productCategory.service';
 
 @Component({
   selector: 'app-product',
@@ -8,18 +10,29 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+
   public products: Product[] = [];
+  public productCategory: ProductCategory[] = [];
+
   constructor(
-    private readonly productService: ProductService
+    private readonly productService: ProductService,
+    private readonly productCategoryService: ProductCategoryService
   ) { }
 
   ngOnInit(): void {
     this.getProductsList();
+    this.getProductCategories();
   }
 
   private getProductsList() {
     this.productService.getProducts().then(data => {
       this.products = data;
+    });
+  }
+
+  private getProductCategories() {
+    this.productCategoryService.getProductCategories().then(data => {
+      this.productCategory = data;
     });
   }
 }
